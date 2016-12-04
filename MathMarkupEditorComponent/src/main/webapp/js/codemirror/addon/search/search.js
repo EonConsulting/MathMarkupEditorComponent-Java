cc48a235b648f2801d6d69c7d1b9d56f7b0e25c7
mbakamotale <mbakaisaac27@gmail.com>
2016-12-04 13:21:08 -0800

    1) Build the UI for MathMarkupComponent-Java

--- MathMarkupEditorComponent/src/main/webapp/js/codemirror/addon/search/search.js
+++ MathMarkupEditorComponent/src/main/webapp/js/codemirror/addon/search/search.js
@@ -1,38 +1,13 @@
-/**
-* Copyright (c) 2016, University of South Africa and/or its affiliates. All rights reserved.
-* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-*
-* This code is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License version 2 only, as
-* published by the Free Software Foundation.
-*
-* This code is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-* version 2 for more details (a copy is included in the LICENSE file that
-* accompanied this code).
-*
-* You should have received a copy of the GNU General Public License version
-* 2 along with this work; if not, write to the Free Software Foundation,
-* Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* @author      Mufaro Benedict
-* @version     1.0 
-**/
+// CodeMirror, copyright (c) by Marijn Haverbeke and others
+// Distributed under an MIT license: http://codemirror.net/LICENSE
 
+// Define search commands. Depends on dialog.js or another
+// implementation of the openDialog method.
 
-/**
- * Define search commands. Depends on dialog.js or another
- *  implementation of the openDialog method.
- *  Replace works a little oddly -- it will do the replace on the next
- *  Ctrl-G (or whatever is bound to findNext) press. You prevent a
- *  replace by making sure the match is no longer selected when hitting
- *  Ctrl-G.
- *  
- * @param {type} mod
- * @returns {undefined}
- */
-
+// Replace works a little oddly -- it will do the replace on the next
+// Ctrl-G (or whatever is bound to findNext) press. You prevent a
+// replace by making sure the match is no longer selected when hitting
+// Ctrl-G.
 
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
